6d3feff203b5dbe9280a4ed638e1aa501894a946
Matheus Ferreira <matheusmlfg@gmail.com>
2023-11-16 20:24:45 -0300

    Implementede LinkedList

---
+++ tests/testLinked_lists.py
@@ -0,0 +1,11 @@
+from data_structures._linkedLists import LinkedList
+
+LList01 = LinkedList()
+LList01.append(1)
+LList01.append(2)
+LList01.append(3)
+LList01.print()
+LList01.pop()
+LList01.pop()
+LList01.pop()
+LList01.print()
